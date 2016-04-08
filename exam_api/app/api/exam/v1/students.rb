867e543cbb68a76197d722693a8100d854001523
Mario Peixoto <mario.peixoto@gmail.com>
2016-04-07 23:18:43 -0500

    Adding students api

--- exam_api/app/api/exam/v1/students.rb
+++ exam_api/app/api/exam/v1/students.rb
@@ -1,8 +1,78 @@
 class Exam::V1::Students < Grape::API
   resource :students do
-    desc 'Return the list of students'
+    desc 'Return all students'
     get do
-      error!("Not implemented")
+      Student.all
+    end
+
+    desc 'Return student with the id provided'
+    params do
+      requires :id, type: Integer, desc: 'Student Id'
+    end
+
+    get ':id' do
+      student = Student.find_by_id(params[:id])
+      if student
+        student
+      else
+        status 404
+      end
+    end
+
+    desc 'Create Student'
+    params do
+      requires :student, type: Hash do
+        requires :username, type: String, desc: 'Username'
+        requires :txstateid, type: String, desc: 'Texas State ID'
+        requires :first_name, type: String, desc: 'First name'
+        requires :last_name, type: String, desc: 'Last name'
+        requires :major, type: String, desc: 'Major'
+        requires :email, type: String, desc: 'Email'
+        requires :phone, type: String, desc: 'Phone'
+      end
+    end
+
+    post do
+      Student.create!(params[:student])
+    end
+
+    desc 'Edit Student'
+    params do
+      requires :id, type: Integer, desc: 'Student Id'
+      requires :student, type: Hash do
+        optional :username, type: String, desc: 'Username'
+        optional :txstateid, type: String, desc: 'Texas State ID'
+        optional :first_name, type: String, desc: 'First name'
+        optional :last_name, type: String, desc: 'Last name'
+        optional :major, type: String, desc: 'Major'
+        optional :email, type: String, desc: 'Email'
+        optional :phone, type: String, desc: 'Phone'
+      end
+    end
+
+    put ':id' do
+      student = Student.find_by_id(params[:id])
+      if student
+        student.update(params[:student])
+        student
+      else
+        status 404
+      end
+    end
+
+    desc 'Remove Student'
+    params do
+      requires :id, type: Integer, desc: 'Student Id'
+    end
+
+    delete ':id' do
+      student = Student.find_by_id(params[:id])
+      if student
+        student.destroy
+        status 200
+      else
+        status 404
+      end
     end
   end
 end
---
+++ exam_api/db/migrate/20160408034531_create_students.rb
@@ -0,0 +1,15 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :username
+      t.string :txstateid
+      t.string :last_name
+      t.string :first_name
+      t.string :major
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
